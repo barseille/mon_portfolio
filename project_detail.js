5217c4bdcb8d76a881cc36cfb677bedfd42fb4d5
BarseillePonn <ponn.barseille@gmail.com>
2023-08-23 13:32:53 +0200

    portfolio permière version

---
+++ project_detail.js
@@ -0,0 +1,47 @@
+// Fonction asynchrone pour charger les détails du projet
+async function loadProjectDetails() {
+    try {
+        // Récupère l'ID du projet depuis l'URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const projectId = urlParams.get('id');
+
+        // Charger le fichier JSON contenant les données des projets
+        const response = await fetch('data.json');
+        const projects = await response.json();
+
+        // Trouver le projet correspondant à l'ID
+        const project = projects.find(p => p.id == projectId);
+
+        // Obtenir l'élément conteneur pour les détails du projet
+        const projectDetailContainer = document.getElementById('project-detail');
+
+        // Diviser la chaîne de caractères "technologies" en un tableau
+        const technologiesArray = project.technologies.split(', ');
+
+        // Créer une chaîne pour stocker les badges de technologie
+        let technologies = "";
+        for (let tech of technologiesArray) {
+            technologies += `<span class="badge badge-info">${tech}</span> `;
+        }
+
+        // Créer les détails du projet HTML
+        const projectDetail = `
+            <h1>${project.title}</h1>
+            <p>${project.description}</p>
+            <p><strong>Technologies :</strong> ${technologies}</p> 
+            <p><strong>Compétences techniques :</strong> ${project.technical_skills}</p>
+            <p><strong>Soft skills :</strong> ${project.soft_skills}</p>
+            <a href="${project.online_link}" class="btn btn-primary" target="_blank">Lien Github</a>
+            <a href="index.html" class="btn btn-success">Retour à la page d'accueil</a>
+        `;
+
+        // Ajouter les détails du projet au conteneur
+        projectDetailContainer.innerHTML = projectDetail;
+    } catch (error) {
+        // Afficher l'erreur dans la console si quelque chose ne va pas
+        console.error('Erreur lors du chargement des détails du projet:', error);
+    }
+}
+
+// Appeler la fonction pour charger les détails du projet
+loadProjectDetails();
---
+++ projects.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", async function() {
+    // Définir le prénom
+    const prenom = "Barseille";
+
+    // Ajouter le prénom à l'élément HTML avec l'ID "prenom"
+    document.getElementById("prenom").innerHTML += prenom;
+
+    // Mettre à jour l'année dans le pied de page
+    document.getElementById('footer-year').innerHTML = "&copy; " + new Date().getFullYear() + " - " + prenom + " Portfolio";
+
+    try {
+        // Récupérer les données JSON depuis le fichier 'data.json'
+        const response = await fetch('data.json');
+        let projects = await response.json();
+
+        // Trier les projets par ordre croissant d'ID
+        projects.sort(function(a, b) {
+            return a.id - b.id;
+        });
+
+        // Obtenir l'élément conteneur pour les projets
+        const projectsContainer = document.getElementById('project-list');
+
+        // Parcourir chaque projet dans le tableau
+        for (let i = 0; i < projects.length; i++) {
+            const project = projects[i];
+
+            // Diviser la chaîne de caractères "technologies" en un tableau
+            const technologiesArray = project.technologies.split(', ');
+
+            // Créer une chaîne vide pour stocker les badges de technologie
+            let technologies = "";
+            for (let j = 0; j < technologiesArray.length; j++) {
+                technologies += '<span class="badge badge-info">' + technologiesArray[j] + '</span> ';
+            }
+
+            const projectCard = `
+                <div class="row mt-4">
+                    <div class="col-md-4">
+                        <img src="${project.image}" alt="${project.title}" class="img-fluid profile-image">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>${project.title}</h3>
+                        <p>${project.description}</p>
+                        <a href="${project.online_link}" class="btn btn-primary" target="_blank">Lien Github</a>
+                        <a href="project_detail.html?id=${project.id}" class="btn btn-success">Détails du projet</a>
+                        <div class="mt-2">${technologies}</div> 
+                    </div>
+                </div>`;
+
+            // Ajouter la carte de projet au conteneur
+            projectsContainer.innerHTML += projectCard;
+        }
+    } catch (error) {
+        // Afficher l'erreur dans la console si quelque chose ne va pas
+        console.error('Erreur lors du chargement des projets:', error);
+    }
+});
